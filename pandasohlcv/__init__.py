e2d463e4311a4d4b0f4653e20778c3897942ab3b
Mohammad <mghmgh1281375@gmail.com>
2023-12-30 20:56:29 +0330

    refactor: :recycle: Add some changes along refactoring.

--- pandasohlcv/__init__.py
+++ pandasohlcv/__init__.py
@@ -2,14 +2,26 @@ import pandas as pd
 import numpy as np
 from pandasohlcv import ohlcv_grouper
 
-__slots__ = ('ohlcv_resampler',)
+__slots__ = ("ohlcv_resampler",)
+
 
 def ohlcv_resampler(df: pd.DataFrame, freq: str) -> pd.DataFrame:
+    """Resamples dataframe to given frequency.
+
+    Args:
+        df (pd.DataFrame): Input dataframe.
+        freq (str): Resampling frequency.
+
+    Returns:
+        pd.DataFrame: Resampled dataframe.
+    """
 
     gpr = df.groupby(pd.Grouper(freq=freq)).grouper
-    labels, obs_group_ids, ngroups = gpr.group_info
+    labels, _, ngroups = gpr.group_info
     out = np.empty((ngroups, 5), dtype=np.float64)
     out = np.ascontiguousarray(out)
-    ohlcv_grouper.ohlcv_grouper(out, np.ascontiguousarray(df.values), np.ascontiguousarray(labels))
+    ohlcv_grouper.ohlcv_grouper(
+        out, np.ascontiguousarray(df.values), np.ascontiguousarray(labels)
+    )
 
     return pd.DataFrame(out, gpr.result_index, columns=df.columns)
--- tests/test_ohlcv.py
+++ tests/test_ohlcv.py
@@ -1,63 +1,61 @@
-from pandasohlcv import ohlcv_resampler
 import time
+import unittest
 import numpy as np
 import pandas as pd
-import unittest
+from pandasohlcv import ohlcv_resampler
 
 
 class OHLCVTestCase(unittest.TestCase):
-
     def __init__(self, methodName: str = "runTest") -> None:
         super().__init__(methodName)
         np.random.seed(42)
         self.cond = dict(
-            open=lambda a: self.asscalar(a.dropna().first('T').values),
+            open=lambda a: self.asscalar(a.dropna().first("T").values),
             high=np.nanmax,
             low=np.nanmin,
-            close=lambda a: self.asscalar(a.dropna().last('T').values),
-            volume=lambda a: np.nansum(a) if not np.isnan(a).all() else np.nan)
-        
-        columns = ['open', 'high', 'low', 'close', 'volume']
-        index = pd.date_range('2023-01-01', '2023-02-02', freq='T')
-        arr = np.random.randint(0, 10, size=(len(index), 5))
-        self.df = pd.DataFrame(arr, index=index, columns=columns).astype('float64')
+            close=lambda a: self.asscalar(a.dropna().last("T").values),
+            volume=lambda a: np.nansum(a) if not np.isnan(a).all() else np.nan,
+        )
 
+        columns = ["open", "high", "low", "close", "volume"]
+        index = pd.date_range("2023-01-01", "2023-02-02", freq="T")
+        arr = np.random.randint(0, 10, size=(len(index), 5))
+        arr = arr.astype(np.float64)
+        self.df = pd.DataFrame(arr, index=index, columns=columns)
 
     @staticmethod
     def asscalar(arr):
-        if len(arr)>0:
+        if len(arr) > 0:
             return np.float64(arr.item())
         else:
             return np.nan
 
-
     def test_ohlcv(self):
-
         t0 = time.time()
-        my_result = ohlcv_resampler(self.df, '5T')
+        my_result = ohlcv_resampler(self.df, "5T")
         print(round(time.time() - t0, 3))
 
         t0 = time.time()
-        pd_result = self.df.resample('5T').agg(self.cond)
+        pd_result = self.df.resample("5T").agg(self.cond)
         print(round(time.time() - t0, 3))
 
         np.testing.assert_array_equal(my_result.values, pd_result.values)
 
-
     def test_ohlcv_nan(self):
-
         df = self.df.copy()
-        nan_mask = np.random.random(df.shape) > .8
+        nan_mask = np.random.random(df.shape) > 0.8
         df.values[nan_mask] = np.nan
 
         t0 = time.time()
-        my_result = ohlcv_resampler(df, '5T')
+        my_result = ohlcv_resampler(df, "5T")
         print(round(time.time() - t0, 3))
 
         t0 = time.time()
-        pd_result = df.resample('5T').agg(self.cond)
+        pd_result = df.resample("5T").agg(self.cond)
         print(round(time.time() - t0, 3))
 
         my_nan_mask = np.isnan(my_result.values)
         pd_nan_mask = np.isnan(pd_result.values)
-        np.testing.assert_array_equal(my_result.values[~my_nan_mask], pd_result.values[~pd_nan_mask])
+        np.testing.assert_array_equal(
+            my_result.values[~my_nan_mask], pd_result.values[~pd_nan_mask]
+        )
